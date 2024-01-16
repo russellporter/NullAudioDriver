f5acec35ac400c20f2e864642373a8d072e8de73
Russell Porter <russell.porter@kaiahealth.com>
2024-01-16 15:21:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+
 sudo xcodebuild -project NullAudio.xcodeproj -target NullAudio DSTROOT="/" install
 sudo launchctl kickstart -kp system/com.apple.audio.coreaudiod || sudo killall coreaudiod
