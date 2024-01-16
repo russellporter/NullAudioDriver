4f55c4f89df090117c965b888a4acaa2ff0e3234
Russell Porter <russell.porter@kaiahealth.com>
2024-01-16 15:43:42 +0100

    Fix build cycle with Xcode 15

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ set -e
 
 cd "$(dirname "$0")"
 
-sudo xcodebuild -project NullAudio.xcodeproj -target NullAudio DSTROOT="/" install
+sudo xcodebuild -project NullAudio.xcodeproj -target NullAudio INSTALL_ROOT="/" install
 sudo launchctl kickstart -kp system/com.apple.audio.coreaudiod || sudo killall coreaudiod
